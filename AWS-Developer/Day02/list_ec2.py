7170e4f731111ba5379a705dfee2b35749087e7a
Ubuntu <ubuntu@ip-172-31-37-191.us-east-2.compute.internal>
2026-07-18 14:45:36 +0000

    List of EC2 properties

--- AWS-Developer/Day02/list_ec2.py
+++ AWS-Developer/Day02/list_ec2.py
@@ -3,4 +3,12 @@ oEC2 = boto3.client("ec2")
 response = oEC2.describe_instances()
 print(type(response))
 print(response.keys())
-
+print(type(response["Reservations"]))
+print(len(response["Reservations"]))
+for eaReservation in response["Reservations"]:
+	print(type(eaReservation))
+	print(eaReservation.keys())
+	print(type(eaReservation["Instances"]))
+	for eaInst in eaReservation["Instances"]:
+		print(type(eaInst))
+		print(eaInst.keys())
