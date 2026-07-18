2116ae66a69a3b84e23fe33f2261023a532ded97
Ubuntu <ubuntu@ip-172-31-37-191.us-east-2.compute.internal>
2026-07-18 23:11:01 +0000

    feat: EC2 Get Inventory Script

--- AWS-Developer/Day02/list_ec2.py
+++ AWS-Developer/Day02/list_ec2.py
@@ -6,10 +6,27 @@ print(response.keys())
 print(type(response["Reservations"]))
 print(len(response["Reservations"]))
 for eaReservation in response["Reservations"]:
-	print("eaReservation", type(eaReservation))
-	print(eaReservation.keys())
-	print("type-> reservation[Instances]", type(eaReservation["Instances"]))
+	print("eaReservation-> ", type(eaReservation))
+	print("eaReservation Keys->", eaReservation.keys())
+	print("type-> reservation[Instances]->", type(eaReservation["Instances"]))
 	for eaInst in eaReservation["Instances"]:
 		print("type-> eaInst", type(eaInst))
-		## print(eaInst.keys())
+		print("*"*40)
+		#print(eaInst.keys())
 		print("Instance Id-> ", eaInst["InstanceId"])
+		print("Instance Type-> ", eaInst["InstanceType"])
+		print("State-> ", eaInst["State"])
+		print("Public IP-> ", eaInst["PublicIpAddress"])
+		print("Private IP-> ", eaInst["PrivateIpAddress"])
+		print("Instance Name->", eaInst["Tags"][0]["Value"] )
+		print("AZ->", eaInst['Placement']['AvailabilityZone'] )
+		print("*"*40)
+		#print("##dir##", dir(eaInst));
+		#print("type eaInst[Tags]->", type(eaInst["Tags"]))
+		#print(eaInst["Tags"][0])
+
+		#for eaTag in eaInst["Tags"]:
+		#	print("Tag ->", type(eaTag));
+		#	print("Tag Keys->", eaTag.keys())
+		#	print("Instance Name", eaTag["Value"])
+
